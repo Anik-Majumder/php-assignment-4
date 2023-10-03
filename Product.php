3f42ea54ed11e589dfb42a1191c4271382926360
Anik Majumder <anikmajumder1592@gmail.com>
2023-10-03 16:35:16 +0600

    final commit

---
+++ Product.php
@@ -0,0 +1,35 @@
+<?php
+
+class Product
+{
+    public $id, $name, $price;
+
+    public function __construct($id, $name, $price)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    private function getFormattedPrice()
+    {
+        $formatedPrice = (string) sprintf("%0.2f", $this->price);
+        $FormatedPriceToString = (string) $formatedPrice;
+        return $FormatedPriceToString;
+    }
+
+
+    public function showDetails()
+    {
+        echo "Product Details: \n";
+        echo "ID: $this->id \n";
+        echo "Name: $this->name \n";
+        echo "Price: " . $this->getFormattedPrice();
+    }
+
+}
+
+$product = new Product(1, 'T-shirt', 19.67461346);
+$product->showDetails();
+
+?>
